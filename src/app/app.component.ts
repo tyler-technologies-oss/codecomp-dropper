b8cbead14b022c443dc70102f65051857d0a31e6
Ryan Sheehan <ryan.sheehan@tylertech.com>
2020-09-03 16:15:57 -0500

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tyl-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'dropper';
+}
