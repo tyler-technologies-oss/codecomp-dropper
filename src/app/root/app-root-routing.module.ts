608654d11e8aa304fb42a65c9e5e19cbbcc142b7
Crook <jerrod.crook@tylertech.com>
2020-09-21 15:33:00 -0600

    Only show registration and about on the live site

--- src/app/root/app-root-routing.module.ts
+++ src/app/root/app-root-routing.module.ts
@@ -4,9 +4,9 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   {path: '', redirectTo: 'about', pathMatch: 'full'},
   {path: 'about', loadChildren: () => import('../about/about.module').then(m => m.AboutModule)},
-  {path: 'game', loadChildren: () => import('../game-host/game-host.module').then(m => m.GameHostModule)},
   {path: 'register', loadChildren: () => import('../registration/registration.module').then(m => m.RegistrationModule)},
-  {path: 'rules', loadChildren: () => import('../rules/rules.module').then(m => m.RulesModule)},
+  /*{path: 'game', loadChildren: () => import('../game-host/game-host.module').then(m => m.GameHostModule)},
+  {path: 'rules', loadChildren: () => import('../rules/rules.module').then(m => m.RulesModule)},*/
   {path: '**', redirectTo: 'about'},
 ];
 
