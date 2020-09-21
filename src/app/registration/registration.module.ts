cbc3700eb29246bbe64b6198f09bd5a63b9d2410
Crook <jerrod.crook@tylertech.com>
2020-09-21 11:22:32 -0600

    embedded registration test

--- src/app/registration/registration.module.ts
+++ src/app/registration/registration.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { RegistrationRoutingModule } from './registration-routing.module';
 import { RegisterComponent } from './register/register.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 
 @NgModule({
   declarations: [RegisterComponent],
   imports: [
     CommonModule,
-    RegistrationRoutingModule
+    RegistrationRoutingModule,
+    FlexLayoutModule
   ]
 })
 export class RegistrationModule { }
