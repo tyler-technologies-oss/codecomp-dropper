80b7fb3693b5c88b805cf2255e82dcab94e7d9b3
Ryan Sheehan <ryan.sheehan@tylertech.com>
2020-10-07 15:54:26 -0500

    refactored how the game is managed from the angular side

--- src/app/game/game.ts
+++ src/app/game/game.ts
@@ -1,13 +1,23 @@
-import { Game, Core } from 'phaser';
+import { Game, Core, Scenes } from 'phaser';
 import { GameConfig, defaultConfig } from './config';
 import { MainScene } from './scenes/main.scene';
 
 export { Game } from 'phaser';
 export { GameConfig } from './config';
+export { Side } from './objects/interfaces';
+export { TeamInfo } from './objects/game-manager';
+
+export const MainKey = 'main';
 
 export const GameEvent = Core.Events;
+export const SceneEvent = Scenes.Events;
 
-export function createGame(config: GameConfig, mainScene:MainScene) {
-  const game = new Game({...defaultConfig, scene: [mainScene], ...config});
-  return game;
+export function createGame(config: GameConfig): Promise<Game> {
+  return new Promise(resolve => {
+    const game = new Game({ ...defaultConfig, scene: [MainScene], ...config });
+    game.events.on(Core.Events.READY, () => {
+      game.scene.pause(MainKey); // immediately pause the game
+      resolve(game);
+    })
+  });
 }
