7e56f2fb80c2f0de56e07d6da520d4ad691a6f03
jerrod.crook <jerrod.crook@tylertech.com>
2020-11-06 15:56:29 -0700

    final additions

--- src/app/tournament/tournament-routing.module.ts
+++ src/app/tournament/tournament-routing.module.ts
@@ -4,18 +4,15 @@ import { TournamentComponent } from './tournament/tournament.component';
 import { BracketComponent } from './bracket/bracket/bracket.component';
 
 const routes: Routes = [
-  {path: '', component: TournamentComponent, children: [
-    { path:'', redirectTo: 'FINALS', pathMatch: 'full' },
-    { path: 'FINALS', component: BracketComponent, data: { key: '8dcacwho' }},
-    { path: 'CSM', component: BracketComponent, data: { key: 'mo72318d' }},
-    { path: 'TTU', component: BracketComponent, data: { key: 'l6pneeks' }},
-    { path: 'CSU', component: BracketComponent, data: {key: 'wb48fauk'}},
-    { path: 'CU', component: BracketComponent, data: {key: 'd6t431mi'}},
-    { path: 'UMaine', component: BracketComponent, data: {key: 'z3i2dq68'}},
-    { path: 'UW', component: BracketComponent, data: {key: '518ltun5'}},
-    { path: 'GATech', component: BracketComponent, data: {key: '28ktglbq'}},
-    { path: 'MISC', component: BracketComponent, data: {key: '2prkbfhc'}}
-  ]}
+  {
+    path: '', component: TournamentComponent, children: [
+      { path: '', redirectTo: 'FINALS', pathMatch: 'full' },
+      { path: 'FINALS', component: BracketComponent, data: { key: '8dcacwho' } },
+      { path: 'CSM', component: BracketComponent, data: { key: 'mo72318d' } },
+      { path: 'TTU', component: BracketComponent, data: { key: 'l6pneeks' } },
+      { path: 'UMaine', component: BracketComponent, data: { key: 'z3i2dq68' } },
+    ]
+  }
 ];
 
 @NgModule({
